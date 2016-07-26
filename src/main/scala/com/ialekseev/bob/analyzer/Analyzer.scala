d783a34349ae39b345f9a72465d9b8872e183069
Igor Alekseev <alekseevi15@gmail.com>
2016-07-26 15:36:39 +0300

    Analyzer: extracting namespace & description

--- src/main/scala/com/ialekseev/bob/analyzer/Analyzer.scala
+++ src/main/scala/com/ialekseev/bob/analyzer/Analyzer.scala
@@ -1,7 +1,7 @@
 package com.ialekseev.bob.analyzer
 
 import com.ialekseev.bob.analyzer.lexical.{AdHocLexicalAnalyzer, LexicalAnalyzer}
-import com.ialekseev.bob.analyzer.syntax.LLSyntaxAnalyzer.ParseTree
+import com.ialekseev.bob.analyzer.syntax.LLSyntaxAnalyzer._
 import com.ialekseev.bob.analyzer.syntax.{AdHocSyntaxAnalyzer, LLSyntaxAnalyzer}
 import org.json4s.JsonAST.JValue
 import scalaz._
@@ -11,17 +11,6 @@ trait Analyzer {
   val lexicalAnalyzer: LexicalAnalyzer
   val syntaxAnalyzer: LLSyntaxAnalyzer
 
-  protected def parse(source: String): AnalysisFailed \/ ParseTree = {
-    lexicalAnalyzer.tokenize(source) match {
-      case \/-(tokens) => syntaxAnalyzer.parse(tokens)
-      case lexFailed@ -\/(_) => lexFailed
-    }
-  }
-
-  protected def mapTreeToAnalysisResult(parseTree: ParseTree): AnalysisResult = {
-    ???
-  }
-
   def analyze(source: String): AnalysisFailed \/ AnalysisResult = {
     require(!source.isEmpty)
 
@@ -29,6 +18,48 @@ trait Analyzer {
       case \/-(parseTree) => mapTreeToAnalysisResult(parseTree).right
       case syntaxFailed@ -\/(_) => syntaxFailed
     }
+  }
+
+  protected def parse(source: String): AnalysisFailed \/ ParseTree = {
+    require(!source.isEmpty)
+
+    lexicalAnalyzer.tokenize(source) match {
+      case \/-(tokens) => syntaxAnalyzer.parse(tokens)
+      case lexFailed@ -\/(_) => lexFailed
+    }
+  }
+
+  protected def mapTreeToAnalysisResult(parseTree: ParseTree): AnalysisResult = {
+    val id: PartialFunction[ParseTreeNode, String] = { case Terminal(LexerToken(Token.Identifier(s), _)) => s }
+    val stringLiteral: PartialFunction[ParseTreeNode, String] = { case Terminal(LexerToken(Token.Type.StringLiteral(s), _)) => s }
+
+    def extractNamespace: Namespace = {
+      val namespace = parseTree.loc.find(_.getLabel == nonTerminal("Namespace"))
+      val namespacePath = namespace >>= (_.find(_.getLabel == nonTerminal("NamespacePath")))
+
+      val namespacePathMain = (namespacePath >>= (_.firstChild)).map(_.getLabel).collect(id).getOrElse(sys.error("Missing namespace main part"))
+      val namespacePathOptional = (namespacePath >>= (_.find(_.getLabel == nonTerminal("NamespacePathParts")).map(parts => {
+        parts.tree.flatten.collect(id)
+      }))).getOrElse(Stream.empty)
+
+      val path = namespacePathOptional.fold(namespacePathMain)(_ + "." + _)
+      val name = (namespace >>= (_.lastChild)).map(_.getLabel).collect(id).getOrElse(sys.error("Missing namespace name"))
+      Namespace(path, name)
+    }
+
+    def extractDescription: String = {
+      (parseTree.loc.find(_.getLabel == nonTerminal("Description")) >>= (_.lastChild)).map(_.getLabel).collect(stringLiteral).getOrElse(sys.error("Missing description"))
+    }
+
+    def extractWebhook: Webhook = {
+      ???
+    }
+
+    val namespace = extractNamespace
+    val description = extractDescription
+
+    //todo: complete
+    AnalysisResult(namespace, description, null, null, null)
   }
 }
 
