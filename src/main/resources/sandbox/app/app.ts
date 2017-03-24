8b8fff96b400cf2534b27f2ebd028bd0b6b88e25
Igor Alekseev <alekseevi15@gmail.com>
2017-03-24 18:26:25 +0300

    Sandbox-js: getting/updating content

--- src/main/resources/sandbox/app/app.ts
+++ src/main/resources/sandbox/app/app.ts
@@ -1,20 +1,47 @@
 import {Component} from 'angular2/core';
-import {Http, HTTP_PROVIDERS} from 'angular2/http'
+import {Http, Headers, HTTP_PROVIDERS} from 'angular2/http'
 import 'rxjs/Rx'
 
 @Component({
-  selector: 'sandbox-app',
-  templateUrl: 'app/app.html',
-  providers: [HTTP_PROVIDERS]
+    selector: 'sandbox-app',
+    templateUrl: 'app/app.html' ,
+    providers: [HTTP_PROVIDERS] //todo: remove?
 })
 export class App {
-  sources: Array<String>;
-  vars: Array<[String, String]>;
+    http: Http
+    sources: Array<string>;
+    variables: Array<NameValuePair<String, String>>;
 
-  constructor(http: Http) {
-     http.get('sandbox/sources').map(r => JSON.parse(r._body)).subscribe(sources => {
-        this.sources = sources.list;
-        this.vars = sources.vars;
-     })
-  }
+    selectedSourceFilePath: string;
+    selectedSourceContent: string;
+
+    private headers = new Headers({'Content-Type': 'application/json'});
+
+    constructor(http: Http) {
+        this.http = http;
+        http.get('sandbox/sources').map(r => JSON.parse(r._body)).subscribe(sources => {
+            this.sources = sources.list;
+            this.variables = sources.vars; //todo: change server-side API so that server-side tuple is serialized into an object having 'name', 'value' properties
+        })
+    }
+
+    onSourceClick(source: string): void {
+        this.http.get(encodeURI('sandbox/sources/' + source)).map(r => JSON.parse(r._body)).subscribe(source => {
+            this.selectedSourceFilePath = source.filePath;
+            this.selectedSourceContent = source.content;
+        })
+    }
+
+    onSaveSelectedSourceClick(): void {
+        this.http.put(encodeURI('sandbox/sources/' + this.selectedSourceFilePath), JSON.stringify({"content": this.selectedSourceContent}), {headers: this.headers}).subscribe(res => {
+            console.log("Updated:")
+            console.log(res)
+        })
+    }
+}
+
+export class NameValuePair<T1, T2> {
+    name: T1;
+    value: T2;
+    constructor(n: T1, v: T2) { this.name = n; this.value = v; }
 }
